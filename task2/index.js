87223be33d104c9fd2dc02f211deace5460c2a81
ADIthaker <adityathaker28@gmail.com>
2020-07-20 09:50:27 +0530

    barebones task2

---
+++ task2/index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const axios = require('axios');
+const app = express();
+
+app.get('/getjokes',(req,res,next)=>{
+    axios.get('https://sv443.net/jokeapi/v2/joke/Any?type=single')
+    .then(response=>{
+        res.status(200).send('<p> Category : '+response.data.category+'<p> <i>'+response.data.joke+'</i><p>id : '+response.data.id+'</p>');
+        console.log(response.data);
+    })
+});
+
+app.listen(5000,()=>{
+    console.log('Listening on port 5000');
+});
