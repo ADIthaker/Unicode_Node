fb1e1d28fa9000c429fe95e9d7c3df20b93d49bb
ADIthaker <adityathaker28@gmail.com>
2020-07-20 10:59:37 +0530

    adding diff routes and templating with basic styles

--- task2/index.js
+++ task2/index.js
@@ -1,18 +1,48 @@
 const express = require('express');
+const path = require('path');
 const axios = require('axios');
+const { response } = require('express');
 const app = express();
 
-app.get('/getjokes',(req,res,next)=>{
+app.set('view engine','ejs');
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/singlejokes',(req,res,next)=>{
     axios.get('https://sv443.net/jokeapi/v2/joke/Any?type=single')
     .then(response=>{
-        res.status(200).send('<p> Category : '+response.data.category+'<p> <i>'+response.data.joke+'</i><p>id : '+response.data.id+'</p>');
-        console.log(response.data);
+        // console.log(response.data);
+        res.render('index',{
+            category:response.data.category,
+            joke:response.data.joke,
+            id:response.data.id,
+            type:response.data.type
+        });
+        return next();
     })
     .catch(err=>{
         res.status(500).send('Oops something went wrong!');
         console.log(err);
     });
 });
+
+app.get('/twopartjokes',(req,res,next)=>{
+    axios.get('https://sv443.net/jokeapi/v2/joke/Any?type=twopart')
+    .then(response=>{
+        res.render('index',{
+            category:response.data.category,
+            setup:response.data.setup,
+            delivery:response.data.delivery,
+            id:response.data.id,
+            type:response.data.type
+        })
+        .catch(err=> 
+        {res.status(500).send('Oops something went wrong!');
+        console.log(err);
+        });
+    })
+})
+
 
 app.listen(5000,()=>{
     console.log('Listening on port 5000');
