eb34e43fa63ebc35d708c2e968524ef2fe76f024
ADIthaker <adityathaker28@gmail.com>
2020-07-20 10:03:21 +0530

    adding minimum error handling

--- task2/index.js
+++ task2/index.js
@@ -8,6 +8,10 @@ app.get('/getjokes',(req,res,next)=>{
         res.status(200).send('<p> Category : '+response.data.category+'<p> <i>'+response.data.joke+'</i><p>id : '+response.data.id+'</p>');
         console.log(response.data);
     })
+    .catch(err=>{
+        res.status(500).send('Oops something went wrong!');
+        console.log(err);
+    });
 });
 
 app.listen(5000,()=>{
